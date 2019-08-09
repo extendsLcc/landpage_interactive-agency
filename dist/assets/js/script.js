0d2137dd56586b78f8ad6f7a753f238aaa036eac
Lucas LCC <lucas.lcc@hotmail.com>
2019-08-09 18:57:46 -0300

    Adicionado Seção Our Works(Portifolio); Minor bugfix css

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -1,13 +1,39 @@
-/*( ()=> {
+(() => {
 
-    $(window).on('scroll', function(e) {
-        var scrollY = $(window).scrollTop();
-        if ( scrollY > 400) {
-             $('#navbar-main').addClass('fixed-top');
-        }else{
-            
-            $('#navbar-main').removeClass('fixed-top');
+    // ! Ref: https://www.codeply.com/go/zjZsn1ly29
+
+    $('#works-carousel').carousel({
+        interval: 10000000
+    })
+
+    $('.carousel .carousel-item').each(function () {
+
+        var minPerSlide = 3;
+        var next = $(this).next();
+
+        if (!next.length) {
+
+            next = $(this).siblings(':first');
 
         }
+
+        next.children(':first-child').clone().appendTo($(this));
+
+        for (var i = 0; i < minPerSlide; i++) {
+
+            next = next.next();
+
+            if (!next.length) {
+
+                next = $(this).siblings(':first');
+
+            }
+
+            next.children(':first-child').clone().appendTo($(this));
+
+        }
+
     });
-})();*/
+
+
+})();
