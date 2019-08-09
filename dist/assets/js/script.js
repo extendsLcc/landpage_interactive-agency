1d315e1d33a085d40fc083807b0985f19e9a06a5
Lucas LCC <lucas.lcc@hotmail.com>
2019-08-09 01:50:22 -0300

    Header finalizado, adicionado estrutura base dos outros componentes

---
+++ dist/assets/js/script.js
@@ -0,0 +1,13 @@
+/*( ()=> {
+
+    $(window).on('scroll', function(e) {
+        var scrollY = $(window).scrollTop();
+        if ( scrollY > 400) {
+             $('#navbar-main').addClass('fixed-top');
+        }else{
+            
+            $('#navbar-main').removeClass('fixed-top');
+
+        }
+    });
+})();*/
